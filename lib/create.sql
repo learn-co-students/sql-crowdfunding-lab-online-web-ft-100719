63a6535f9f006ec76cc651e7d4fc49b5b10493c6
jacquibushfi <jacqui@stainedglassofmckinney.com>
2019-11-04 08:29:43 -0600

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,23 @@
-CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal REAL, start_date_text TEXT, end_date_text TEXT);
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
+);
 
-CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT,age TEXT);
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
 
-CREATE TABLE pledge (id INTEGER PRIMARY KEY, amount REAL, user_id INTEGER, project_id INTEGER)
-
-
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+);
 
 
